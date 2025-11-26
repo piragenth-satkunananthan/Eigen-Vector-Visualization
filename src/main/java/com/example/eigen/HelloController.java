a1a0b7670fe59ed13bed2ab3229f040d0ae3eb01
piragenthuni <piragenthuni@gmail.com>
2025-11-27 02:21:06 +0530

    drawed arrow almost base calculated eigen value

--- src/main/java/com/example/eigen/HelloController.java
+++ src/main/java/com/example/eigen/HelloController.java
@@ -1,7 +1,9 @@
 package com.example.eigen;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.layout.Pane;
@@ -10,18 +12,91 @@ import javafx.scene.layout.Pane;
 public class HelloController {
     @FXML
     private Pane pane;
+    @FXML
+    private TextField v1;
+    @FXML
+    private TextField v2;
+
+    @FXML
+    private TextField a;
+    @FXML
+    private TextField b;
+    @FXML
+    private TextField c;
+    @FXML
+    private TextField d;
 
     public void initialize() {
+        Platform.runLater(() -> {
+            manager();
+        });
+    }
+
+    public void manager() {
+//        pane.widthProperty().addListener((o,ov,nv) -> drawBaseAxis());
+//        pane.heightProperty().addListener((o,ov,nv) -> drawBaseAxis());
+        drawBaseAxis();
+
+
+        v1.textProperty().addListener((o, ov, nv) -> {
+            int vector1 = Integer.parseInt(v1.getText());
+            int vector2 = Integer.parseInt(v2.getText());
+            pane.getChildren().clear();
+            draw(vector1, vector2);
+        });
+
+        v2.textProperty().addListener((o, ov, nv) -> {
+            int vector1 = Integer.parseInt(v1.getText());
+            int vector2 = Integer.parseInt(v2.getText());
+            pane.getChildren().clear();
+            draw(vector1, vector2);
+        });
 
     }
-    public void draw(){
-        Line line = new Line(0,pane.getHeight(),200,pane.getHeight()-200);
+
+    public void drawBaseAxis() {
+        pane.setManaged(false);
+
+        double widthPane = pane.getWidth();
+        double heightPane = pane.getHeight();
+//        System.out.println(heightPane);
+//        System.out.println(widthPane);
+
+        Line baseXaxis = new Line(0, heightPane, widthPane, heightPane);
+        baseXaxis.setStroke(Color.BLACK);
+        baseXaxis.setStrokeWidth(3);
+
+        Line baseYaxis = new Line(0, 0, 0, heightPane);
+        baseYaxis.setStroke(Color.BLACK);
+        baseYaxis.setStrokeWidth(3);
+        pane.getChildren().addAll(baseXaxis, baseYaxis);
+    }
+
+    public void draw(double vector1, double vector2) {
+        drawBaseAxis();
+        Line line = new Line(0, pane.getHeight(), vector1, pane.getHeight() - vector2);
         line.setStroke(Color.BLUE);
         line.setStrokeWidth(2);
         pane.getChildren().add(line);
     }
 
-    public void CalculateEigen(){
+    public void CalculateEigen() {
+        int ma = Integer.parseInt(a.getText());
+        int mb = Integer.parseInt(b.getText());
+        int mc = Integer.parseInt(c.getText());
+        int md = Integer.parseInt(d.getText());
+
+        float firstSec = ma+md;
+        double secondSec =  Math.sqrt((ma+md)*(ma+md)-4*(ma*md-mb*mc));
+
+        double firstVariation = (firstSec+secondSec)/2*ma;
+
+        double secondVariation = (firstSec+(-1*(secondSec)))/2*ma;
+
+        System.out.println(firstVariation);
+        System.out.println(secondVariation);
+
+
 
     }
 }
