9f09a0e44545ca22974f7a2666b399bea9773abc
piragenthuni <piragenthuni@gmail.com>
2025-11-30 02:41:31 +0530

    eigen calculation done need

--- src/main/java/com/example/eigen/HelloController.java
+++ src/main/java/com/example/eigen/HelloController.java
@@ -2,9 +2,10 @@ package com.example.eigen;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.geometry.Point2D;
 import javafx.scene.Group;
 
-import javafx.scene.Parent;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
@@ -14,7 +15,7 @@ import javafx.scene.transform.Rotate;
 
 public class HelloController {
     @FXML
-    private  Pane pane;
+    private Pane pane;
     @FXML
     private TextField a;
     @FXML
@@ -23,13 +24,21 @@ public class HelloController {
     private TextField c;
     @FXML
     private TextField d;
-    public static double scalar = 40;
+
+    public static double scalar = 100;
 
     public void initialize() {
 
-        Platform.runLater(() -> {
-            drawBaseAxis();
-        });
+        Platform.runLater(() -> manger());
+    }
+
+    public void manger() {
+        drawBaseAxis();
+        int ma = Integer.parseInt(a.getText());
+        int mb = Integer.parseInt(b.getText());
+        int mc = Integer.parseInt(c.getText());
+        int md = Integer.parseInt(d.getText());
+        CalculateEigenValue(ma, mb, mc, md);
     }
 
     public void drawBaseAxis() {
@@ -37,43 +46,52 @@ public class HelloController {
 
         double widthPane = pane.getWidth();
         double heightPane = pane.getHeight();
+        Label x_left = new Label("(-"+String.valueOf(widthPane/2)+")");
+        Label x_right = new Label("("+String.valueOf(widthPane/2)+")");
+
+        Label y_top = new Label("("+String.valueOf(heightPane/2)+")");
+        Label y_down = new Label("(-"+String.valueOf(heightPane/2)+")");
+
 //        System.out.println(heightPane);
 //        System.out.println(widthPane);
 
-        Line baseXaxis = new Line(0, heightPane, widthPane, heightPane);
+        Line baseXaxis = new Line(0, heightPane/2, widthPane, heightPane/2);
+        x_left.setTranslateY(heightPane/2);
+        x_right.setTranslateY(heightPane/2);
+        x_right.setTranslateX(widthPane);
+
+        y_top.setTranslateX(widthPane/2);
+        y_down.setTranslateX(widthPane/2);
+        y_down.setTranslateY(heightPane);
         baseXaxis.setStroke(Color.BLACK);
         baseXaxis.setStrokeWidth(1);
 
-        Line baseYaxis = new Line(0, 0, 0, heightPane);
+        Line baseYaxis = new Line(heightPane/2, 0, widthPane/2, heightPane);
         baseYaxis.setStroke(Color.BLACK);
         baseYaxis.setStrokeWidth(1);
-        pane.getChildren().addAll(baseXaxis, baseYaxis);
+        pane.getChildren().addAll(baseXaxis, baseYaxis,x_left,x_right,y_down,y_top);
     }
 
-    public void draw(double vector1, double vector2) {
-        pane.getChildren().clear();
+
+    public void draw(double x1, double x2) {
         drawBaseAxis();
-        Vector vector = new Vector(vector1,vector2);
+        Vector vector = new Vector(x1, x2);
 //        Line line = new Line(0, pane.getHeight(), vector1*scalar, pane.getHeight() - vector2*scalar);
 //        line.setStroke(Color.BLUE);
 //        line.setStrokeWidth(1);
 
-        double angle = Math.toDegrees(Math.atan2(-vector2,vector1));
-        System.out.println(angle);
+        double angle = Math.toDegrees(Math.atan2(-x2, x1));
+        System.out.println("Angle :-" + angle);
 //        vector.setTranslateX(0);
-        vector.setTranslateY(pane.getHeight());
+        vector.setTranslateY(pane.getHeight()/2);
+        vector.setTranslateX(pane.getWidth()/2);
 //        vector.setRotate(angle);
-
-        Rotate rotate = new Rotate(angle,vector1,vector2);
+        Rotate rotate = new Rotate(angle, 0, 0);
         vector.getTransforms().add(rotate);
         pane.getChildren().add(vector);
     }
 
-    public void CalculateEigen() {
-        int ma = Integer.parseInt(a.getText());
-        int mb = Integer.parseInt(b.getText());
-        int mc = Integer.parseInt(c.getText());
-        int md = Integer.parseInt(d.getText());
+    public void CalculateEigenValue(int ma, int mb, int mc, int md) {
 
 //        int ma = 1;
 //        int mb = 2;
@@ -81,48 +99,87 @@ public class HelloController {
 //        int md = 1;
         double checkValid = (ma + md) * (ma + md) - 4 * ma * md - mb * mc;
 
-        if (checkValid < 0) {
-            System.out.println("No Eigen Vector in this transfomation");
+
+        double firstSec = ma + md;
+        double secondSec = Math.sqrt((ma + md) * (ma + md) - 4 * (ma * md - mb * mc));
+
+        double eigenValue1 = (firstSec + secondSec) / 2;
+
+        double eigenValue2 = (firstSec - secondSec) / 2;
+
+        System.out.println("Eigen Value 1 :- " + eigenValue1);
+        System.out.println("Eigen Value 2 :- " + eigenValue2);
+
+        //need to check for eigen vlalue valid
+        if (checkValid > 0) {
+            CalculateEigenVector(eigenValue1, ma, mb, mc, md);
+            CalculateEigenVector(eigenValue2, ma, mb, mc, md);
+        } else if (checkValid == 0) {
+
+
+            CalculateEigenVector(eigenValue1, ma, mb, mc, md);
         } else {
-            double firstSec = ma + md;
-            double secondSec = Math.sqrt((ma + md) * (ma + md) - 4 * (ma * md - mb * mc));
-
-            double eigenValue1 = (firstSec + secondSec) / (2 * ma);
-
-            double eigenValue2 = (firstSec + (-1 * (secondSec))) / (2 * ma);
-
-            System.out.println(eigenValue1);
-            System.out.println(eigenValue2);
-
-            double x1=1, x2, y1 = 1, y2;
-
-            x2 = -1*(ma - eigenValue1)/mb * x1;
-
-            draw(x1, x2);
-
+            System.out.println("No Transformation");
         }
 
+
+        if ((ma == eigenValue1) & (md == eigenValue2)) {
+            //working with sclaing equaly matrix
+            System.out.println("Working with scalling equally matrix working later after vector implemented");
+        }
+
+
+    }
+
+    public void CalculateEigenVector(double eigenValue, int ma, int mb, int mc, int md) {
+        pane.getChildren().clear();
+
+        double x1 = 0, x2 = 1;
+
+
+        if ((mb == 0) || (mc == 0)) {
+            if (ma == eigenValue) {
+                //x1 is free variable so it can go x_axis any long
+                x1 = 1;
+                x2 = 0;
+            } else if (md == eigenValue) {
+                //x2 is free variable so it can go y_axis
+                x2 = 1;
+                x1 = 0;
+            }
+        } else {
+            x1 = (mb * x2) / ma - eigenValue;
+        }
+
+        System.out.println("Eigen Vectors :- [" + x1 + "," + x2 + "]");
+
+        draw(x1, x2);
     }
 
     public static class Vector extends Group {
 
-        Vector(double vector1, double vector2){
+        Vector(double x1, double x2) {
+            Point2D eigen_vector = new Point2D(x1, x2);
+            double magnitude = eigen_vector.magnitude();
 
-            Line lineT = new Line(vector1*scalar,5,(vector1*scalar)-6,0);
-            Line lineM = new Line(0,0,vector1*scalar,5);
-            Line lineD = new Line(vector1*scalar,5,(vector1*scalar)-6,10);
 
+            int head_size = 5;
+
+            Line lineT = new Line(magnitude * scalar, 0, (magnitude * scalar) - head_size, head_size);
+            Line lineM = new Line(0, 0, magnitude * scalar, 0);
+            Line lineD = new Line(magnitude * scalar, 0, (magnitude * scalar) - head_size, -head_size);
 
 
             lineT.setStroke(Color.BLUE);
             lineD.setStroke(Color.BLUE);
             lineM.setStroke(Color.BLUE);
 
-            lineT.setStrokeWidth(1);
-            lineD.setStrokeWidth(1);
-            lineM.setStrokeWidth(1);
+            double stroke_width = 2;
+            lineT.setStrokeWidth(stroke_width);
+            lineD.setStrokeWidth(stroke_width);
+            lineM.setStrokeWidth(stroke_width);
 
-            getChildren().addAll(lineT,lineM, lineD);
+            getChildren().addAll(lineT, lineM, lineD);
         }
     }
 }
